ff1bb653863d39f0183c2e57c15a3425d63d3171
Pasatino <andreipasatino@gmail.com>
2024-07-21 13:59:31 +0200

    aggiunto un filtro per categoria, non ancora terminato

--- articoli.js
+++ articoli.js
@@ -1,7 +1,6 @@
 let navbar = document.querySelector("#navbar")
-
 let article = document.querySelector("#article")
-
+let accordionBody = document.querySelector("#acordionBody")
 window.addEventListener("scroll", () => {
     let scrolled = window.scrollY;
     if (scrolled > 0){
@@ -20,28 +19,54 @@ window.addEventListener("scroll", () => {
 fetch('./annunci.json')
     .then((response)=>response.json())
     .then((data)=>  {
+        
+        //ho creato questa funzione per ridurre i nomi lunghi degli articoli
         function truncateWord(string) {
             if (string.length > 15) {
-              return string.split(" ")[0] + "...";
+                return string.split(" ")[0] + "...";
             } else {
-              return string;
+                return string;
             }
         }
-    function showCards(data){
-        data.forEach((articles)=>{
-            let card = document.createElement('article');
-            card.classList.add('col-3')
-            card.innerHTML =`
-            <article class='flex-column p-3      article-body bg-primary textFont1'>
-            <h3 class="articleName">${articles.name}</h3>
-            <p class="articleCategory">${articles.category}</p>
-            <p class="articlePrice">${articles.price}</p> </article>`
-            article.appendChild(card); //appendChild ci permette di inserire le card create all'interno del section con id article           
+        function showCards(data){
+            data.forEach((articles)=>{
+                let card = document.createElement('article');
+                card.classList.add('col-3')
+                card.innerHTML =`
+                <article class='flex-column p-3      article-body bg-primary textFont1'>
+                <h3 class="articleName">${truncateWord(articles.name)}</h3>
+                <p class="articleCategory">${articles.category}</p>
+                <p class="articlePrice">${articles.price} $</p> </article>`
+                article.appendChild(card); //appendChild ci permette di inserire le card create all'interno del section con id article           
         });
+
+        function generateRadios() {
+            let categories = data.map((annuncio)=> annuncio.category);
+            let uniqueCategories = [];
+            categories.forEach((category)=>{
+                if(!uniqueCategories.includes(category)){
+                    uniqueCategories.push(category);
+                }
+            });
+            console.log(uniqueCategories);
+            uniqueCategories.forEach((category)=>{
+                let div = document.createElement('div');
+                div.classList.add('form-check');
+                div.innerHTML =`
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Default radio
+                    </label>
+                    `;
+                accordionBody.appendChild(div);
+            })
+        }
+        generateRadios();
         
     }
+    
+    
     console.log(data);
     showCards(data)
 })
 
-showCards(data)
